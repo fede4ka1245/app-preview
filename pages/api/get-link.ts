9a81c1389a3019008677b76b1825a322b6ecc513
fede4ka1245 <bragin.fedor96@gmail.com>
2023-04-04 22:49:16 +0500

    fix get-link

--- pages/api/get-link.ts
+++ pages/api/get-link.ts
@@ -16,7 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return
   }
 
-  const { key } = req.query;
+  const { key } = req.body;
 
   const link = await prisma.link.findFirst({
     where: {
